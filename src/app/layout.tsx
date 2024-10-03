e1b8b8bcad23dd9921af52153bc81185a87bfa2d
Godcoder696 <117354227+Godcoder696@users.noreply.github.com>
2024-10-03 22:12:16 +0530

    Google analytics added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react"
 import {AppWrapper} from './context/ContextProvider'
+import GAnalytics from "@/components/GAnalytics";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <GAnalytics/>
         <AuthProvider>
           <AppWrapper>
             <NavBar/>
