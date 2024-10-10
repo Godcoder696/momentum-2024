cc102c434e374b64ab39b0b6e4f074455d0a99dc
Godcoder696 <117354227+Godcoder696@users.noreply.github.com>
2024-10-10 20:11:33 +0530

    phone number bug fixed

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -6,7 +6,7 @@ import college from "../data/college.json";
 function UserProfile({ usrDetails }) {
   const [fname, setFname] = useState();
   const [lname, setLname] = useState();
-  const [phNum, setPhNum] = useState("");
+  const [phNum, setPhNum] = useState();
   const [clgName, setClgName] = useState();
   const [id, setId] = useState();
   const [role, setRole] = useState("");
@@ -20,7 +20,7 @@ function UserProfile({ usrDetails }) {
     if (usrDetails) {
       setFname(usrDetails.fname);
       setLname(usrDetails.lname);
-      setPhNum(usrDetails.pNumber);
+      setPhNum(usrDetails.pNumber + "");
       setClgName(usrDetails.collegeName);
       setId(usrDetails._id);
       setYear(usrDetails.year || 1);
