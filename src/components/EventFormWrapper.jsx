5221dd3e8cd7913f5c2f286f0193c335fef42983
Godcoder696 <117354227+Godcoder696@users.noreply.github.com>
2024-10-12 01:23:04 +0530

    minor bug fixed

--- src/components/EventFormWrapper.jsx
+++ src/components/EventFormWrapper.jsx
@@ -30,8 +30,10 @@ function EventFormWrapper({ eventId }) {
   }, [maxSize]);
   const handlePayment = async () => {
     let isValid = true;
-    if (!tc) isValid = myToast("Please accept terms & conditions.");
-    isValid = tc;
+    if (!tc) {
+      myToast("Please accept terms & conditions.");
+      isValid = tc;
+    }
     if (events[eventId].type == "Team") isValid = validateDetails();
     if (isValid) {
       setIsProcessing(true);
