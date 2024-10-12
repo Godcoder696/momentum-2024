a94a0fcaa85cf7415aba065a24b5071823b10c8e
Godcoder696 <117354227+Godcoder696@users.noreply.github.com>
2024-10-13 01:09:08 +0530

    CORS setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
+const nextConfig = {};
 
 export default nextConfig;
-const nextConfig = {
+nextConfig = {
     async headers() {
         return [
             {
