1abf77d4a8a02a26cfb8c1e505f5ff2b1995d70f
Godcoder696 <117354227+Godcoder696@users.noreply.github.com>
2024-10-20 18:43:04 +0530

    sponsors heading changed

--- src/components/Sponsors.tsx
+++ src/components/Sponsors.tsx
@@ -6,7 +6,7 @@ function Sponsors() {
     <>
       <div className=' bg-[#030919] -mt-2 pt-7 pb-10 xl:pb-44 flex flex-col items-center justify-between w-screen space-y-10 xl:space-y-20'>
         <div className='flex justify-start text-white w-[90%] text-lg xl:text-3xl font-semibold'>
-          <span>SPONSORED BY</span>
+          <span>OUR SPONSORS</span>
         </div>
         <Image src="/Sponsors.svg" height={750} width={750} alt='sponsors' className='p-4'/>
       </div> 
